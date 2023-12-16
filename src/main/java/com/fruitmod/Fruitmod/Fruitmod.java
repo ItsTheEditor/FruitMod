61c2e23cbedad10230ded9822c84d9b8dcfa6154
ItsTheEditor <editspace90@gmail.com>
2023-12-16 15:13:51 -0800

    added modid

--- src/main/java/com/fruitmod/Fruitmod/Fruitmod.java
+++ src/main/java/com/fruitmod/Fruitmod/Fruitmod.java
@@ -14,12 +14,14 @@ public class Fruitmod
     public static final String NAME = "Fruitmod";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
+    public static Fruitmod Instance;
+    public static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+
     }
 
     @EventHandler
