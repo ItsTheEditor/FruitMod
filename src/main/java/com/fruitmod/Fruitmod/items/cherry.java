97f2aca0dbd0db840b06493c2bb0f9e55809a5a9
ItsTheEditor <editspace90@gmail.com>
2023-12-17 11:53:11 -0800

    fixing bugs

--- src/main/java/com/fruitmod/Fruitmod/items/cherry.java
+++ src/main/java/com/fruitmod/Fruitmod/items/cherry.java
@@ -6,9 +6,10 @@ import com.fruitmod.Fruitmod.Fruitmod;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Objects;
 
 public class cherry extends Item {
 
@@ -19,6 +20,6 @@ public class cherry extends Item {
     }
     @SideOnly(Side.CLIENT)
     public void initModel(){
-        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(Objects.requireNonNull(getRegistryName()),"inventory"));
     }
 }
