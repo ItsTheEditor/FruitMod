e286c7864b63a873db3043a2001c1cd63e99fbe9
ItsTheEditor <editspace90@gmail.com>
2023-12-21 07:29:23 -0800

    adding first item

--- src/main/java/com/fruitmod/Fruitmod/items/ItemBase.java
+++ src/main/java/com/fruitmod/Fruitmod/items/ItemBase.java
@@ -6,21 +6,29 @@ import net.minecraft.item.Item;
 
 public class ItemBase extends Item {
 
-    protected String name;
-    public ItemBase(String name){
-        this.name =  name;
-        setUnlocalizedName(name);
-        setRegistryName(name);
+
+        protected String name;
+
+        public ItemBase(String name) {
+            this.name = name;
+            setUnlocalizedName(name);
+            setRegistryName(name);
+
+            setCreativeTab(CreativeTabs.MISC);
+        }
+
+        public void registerItemModel() {
+            Fruitmod.proxy.registerItemRenderer(this, 0, name);
+        }
+
+        @Override
+        public ItemBase setCreativeTab(CreativeTabs tab) {
+            super.setCreativeTab(tab);
+            return this;
+        }
 
     }
 
-    public void registerItemModel() {
-        Fruitmod.proxy.registerItemRenderer(this, 0, name);
-    }
 
-    @Override
-    public ItemBase setCreativeTab(CreativeTabs tab) {
-        super.setCreativeTab(tab);
-        return this;
-    }
-}
+
+
