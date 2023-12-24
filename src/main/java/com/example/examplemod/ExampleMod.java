b99684d39c76dacf7adf3a2e61871ad738983cbb
ItsTheEditor <editspace90@gmail.com>
2023-12-24 11:09:52 -0800

    restarted

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.editspace.fruitmod;
+package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = fruitmod.MODID, name = fruitmod.NAME, version = fruitmod.VERSION)
-public class fruitmod
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
 {
-    public static final String MODID = "editspace";
-    public static final String NAME = "Fruitmod";
-    public static final String VERSION = "0.0.1";
+    public static final String MODID = "examplemod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0";
 
     private static Logger logger;
 
